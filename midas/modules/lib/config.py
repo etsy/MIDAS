3427f6b92c39b7dc75f354714c33d691103fec41
Marcin Wielgoszewski <marcin.wielgoszewski@gmail.com>
2013-12-07 11:16:44 -0500

    make config a dict, not a class

--- midas/modules/lib/config.py
+++ midas/modules/lib/config.py
@@ -5,66 +5,48 @@ This is the config for MIDAS
 from os.path import dirname, realpath
 
 
-class Config():
-    """the main config class"""
+config = {}
 
-    def __init__(self):
-        pass
+current_dir = dirname(realpath(__file__))
+if "/Users" in current_dir:
+    config['database'] = 'midas_hids.sqlite'
+else:
+    config['database'] = '/tmp/midas_hids.sqlite'
 
-    config = {}
+config['plist_check_keys'] = [
+    'RunAtLoad',
+    'WatchPaths',
+    'KeepAlive',
+    'StartInterval',
+    'StartOnMount',
+    'OnDemand',
+    'QueueDirectories',
+    'StandardInPath',
+    'StandardOutPath',
+    'StandardErrorPath',
+    'Debug',
+    'LaunchOnlyOnce',
+    'Sockets',
+    'OSAXHandlers',
+    'LSEnvironment',
+    'CFBundleVersion',
+]
 
-    current_dir = dirname(realpath(__file__))
-    if "/Users" in current_dir:
-        config['database'] = 'midas_hids.sqlite'
-    else:
-        config['database'] = '/tmp/midas_hids.sqlite'
+config['plist_check_keys_hash'] = [
+    'Program',
+    'ProgramArguments'
+]
 
-    config['plist_check_keys'] = [
-        'RunAtLoad',
-        'WatchPaths',
-        'KeepAlive',
-        'StartInterval',
-        'StartOnMount',
-        'OnDemand',
-        'QueueDirectories',
-        'StandardInPath',
-        'StandardOutPath',
-        'StandardErrorPath',
-        'Debug',
-        'LaunchOnlyOnce',
-        'Sockets',
-        'OSAXHandlers',
-        'LSEnvironment',
-        'CFBundleVersion',
-    ]
+config['firewall_keys'] = [
+    'allowsignedenabled',
+    'firewallunload',
+    'globalstate',
+    'loggingenabled',
+    'previousonstate',
+    'stealthenabled',
+    'version',
+]
 
-    config['plist_check_keys_hash'] = [
-        'Program',
-        'ProgramArguments'
-    ]
 
-    config['firewall_keys'] = [
-        'allowsignedenabled',
-        'firewallunload',
-        'globalstate',
-        'loggingenabled',
-        'previousonstate',
-        'stealthenabled',
-        'version',
-    ]
-
-    @staticmethod
-    def get(key, config=config):
-        """simple get method for Config class"""
-        try:
-            return config[key]
-        except KeyError:
-            return None
-
-    @staticmethod
-    def exists(key, config=config):
-        """simple exists method for Config class"""
-        if key in config.keys():
-            return True
-        else:
-            return False
+# Maintain backwards compatibility
+Config = config
